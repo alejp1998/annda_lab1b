c83c8e967d0382dbcb61d5e13d0cbffb410601db
Jonne van Haastregt <jonnevanhaastregt@gmail.com>
2022-02-10 01:44:41 +0100

    3.1 batch mode training using least squares used and code for the delta rule and CL initialisation. For some reason lab2.delta_rule throws an error that lab2 does not have a module named delta_rule, even though the other modules work.

--- lab2.py
+++ lab2.py
@@ -1,4 +1,58 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-## Generating data
-x = np.linspace(0, 2*np.pi,0.01)
+def RBF (x, mu, sigma): #Kind of redundant, if input is defined correctly RBF_multdim can be used for 1D as well
+    try: 
+        phi = np.zeros((len(x),len(mu))) #For some reason len of 1x1 vector throws an error
+    except:
+        phi = np.zeros((1,len(mu))) #If single data points are fed
+
+    for i in range(len(mu)):
+        phi[:,i] = np.exp(-((x-mu[i])**2)/(2*sigma[i]**2))
+    return phi
+
+def RBF_multdim (x,mu,sigma):
+    phi = np.zeros((x.shape[-1],mu.shape[-1])) #pre-allocate
+    for i in range(mu.shape[-1]): #for all RBF's
+        mu_col = np.array([mu[:,i]]).T #Needed because even though mu[:,i] is a column vector, it is automatically transformed to row vector by numpy
+        phi[:,i] = np.exp(-(np.linalg.norm(x-mu_col)**2)/(2*sigma[:,i]**2))
+    return phi
+
+def least_squares (x,y_train,mu,sigma):
+    phi = RBF(x,mu,sigma)
+    w = np.transpose(phi).dot(y_train).dot(np.linalg.inv(np.dot(np.transpose(phi),phi)))
+    return w
+
+def delta_rule (x,y_train,mu,sigma,lr,epochs):
+    w = np.random.normal(0,1,N)
+    for i in range(epochs):
+        ind = np.random.permutation(x.shape[-1]) #shuffle data order
+        x =  x[ind]
+        y_train = y_train[ind]
+        for k in range(x.shape[-1]):
+            phi = RBF(x[k],mu,sigma)
+            phi = phi[0,:]
+            w = w + lr*(y_train[k] - np.sum(w*phi))*phi
+    return w
+
+def forward (x,w,mu,sigma):
+    y = np.sum(w*RBF(x,mu,sigma),-1)
+    return y
+
+def CL_initialisation (x,dim,N,s,epochs,n_winners,lr):
+    # x has dim rows and N_datapoints columns 
+    # mu has dim rows and N_RBF (N) columns
+    # sigma has 1 row with N_RBF (N) columns (std equal in all directions)
+    mu = np.random.uniform(x.min(-1),x.max(-1),(dim,N)) #initialise RBF's randomly uniform
+    sigma = s*np.ones((1,N))
+
+    for k in range(epochs):
+        x = x[:, np.random.permutation(x.shape[-1])] #shuffle data order
+        for i in range(x.shape[-1]):
+            phi = RBF_multdim(x[:,i],mu,sigma)
+            for j in range(n_winners):
+                biggest = max(phi)
+                biggest_ind = phi.index(biggest)
+                mu[:,biggest_ind] = mu[:,biggest_ind] + lr/j(x[:,i] - mu[:,biggest_ind]) #move RBF closer to data point
+
+    return mu, sigma
